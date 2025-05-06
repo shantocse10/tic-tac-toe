78a2058569977bdf87a1571102818440d05af29f
Touhidur Rahman <shantotrs@gmail.com>
2025-05-06 14:43:40 +0600

    implement game logic and update Square component for click handling

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,24 +1,39 @@
 import Square from "./Square.jsx";
+import {useState} from "react";
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  function handleClick(i) {
+    if (squares[i]) return;
+
+    const newSquares = squares.slice();
+
+    newSquares[i] = xIsNext ? 'X' : 'O';
+
+    setSquares(newSquares);
+    setXIsNext(!xIsNext);
+  }
+
   return (
     <>
-      <div>
-        <Square value={1} />
-        <Square value={2} />
-        <Square value={3} />
+      <div className="flex">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
 
-      <div>
-        <Square value={4} />
-        <Square value={5} />
-        <Square value={6} />
+      <div className="flex">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
 
-      <div>
-        <Square value={7} />
-        <Square value={8} />
-        <Square value={9} />
+      <div className="flex">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,7 +1,10 @@
-export default function Square({value}) {
+export default function Square({value, onSquareClick}) {
+  const textColor = value === 'X' ? 'text-red-500' : value === 'O' ? 'text-blue-500' : '';
+
   return (
     <>
-      <button className="bg-blue-200 border border-blue-200 rounded-md w-16 h-16 m-1 text-2xl leading-9 shadow-sm shadow-blue-100">
+      <button className={`bg-blue-200 border border-blue-200 rounded-md w-16 h-16 m-1 text-3xl font-bold leading-9 shadow-sm shadow-blue-100 ${textColor}`}
+              onClick={onSquareClick}>
         {value}
       </button>
     </>
